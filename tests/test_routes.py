24de015eed21dccf5eea3b8969576caaaf073d63
LeykenKing <116745629+LeykenKing@users.noreply.github.com>
2026-05-07 17:36:08 +0100

    Implement test for deleting a product
    Added a test to verify product deletion functionality.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -180,7 +180,20 @@ class TestProductRoutes(TestCase):
         # get the data from resp.get_json()
         # assert that data["name"] equals the test_product.name
   
-  
+      def test_delete_product(self):
+        """It should Delete a Product"""
+        products = self._create_products(5)
+        product_count = self.get_product_count()
+        test_product = products[0]
+        response = self.client.delete(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        # make sure they are deleted
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        new_count = self.get_product_count()
+        self.assertEqual(new_count, product_count - 1)
+
     #
 
     ######################################################################
