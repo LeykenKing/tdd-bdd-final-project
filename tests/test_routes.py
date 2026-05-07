8d51d133d8af992a42da91f8477a2b231aa03f1d
LeykenKing <116745629+LeykenKing@users.noreply.github.com>
2026-05-07 16:56:23 +0100

    Implement test for getting a single product
    Add a test case for retrieving a single product.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -165,6 +165,22 @@ class TestProductRoutes(TestCase):
 
     #
     # ADD YOUR TEST CASES HERE
+    def test_get_product(self):
+         """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = res.get_json()
+        sef.assertEqual(data{"name"], test_product.name)
+
+
+        # make a self.client.get request to the API endpoint and store the result in the variable named response
+        # assert that the resp.status_code is status.HTTP_200_OK
+        # get the data from resp.get_json()
+        # assert that data["name"] equals the test_product.name
+  
+  
     #
 
     ######################################################################
